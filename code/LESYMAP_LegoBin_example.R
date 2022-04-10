5ff33909ed66029f011c955c1d1d0439ed844fa9
ljhearne <luke.hearne@qimrberghofer.edu.au>
2022-04-11 09:33:25 +1000

    cleaned up + added bonf

--- code/LESYMAP_LegoBin_example.R
+++ code/LESYMAP_LegoBin_example.R
@@ -1,49 +1,52 @@
+# This code uses the LESYMAP toolbox to run a univariate VLSM
+# on regular lesion data and connectivity lesion data.
+# I've just ran univariate methods because the multivariate method
+# (sparse CCA) is really slow.
+
 library(LESYMAP)
 
 # project directory
-proj_dir = '/home/lukeh/projects/NeglectNetworks/'
+proj_dir <- "/home/lukeh/projects/NeglectNetworks/"
 
 # participant information spreadsheet
-df_file = paste(proj_dir,'data/derivatives/participant_dataframe.csv', sep='')
+df_file <- paste(proj_dir, "data/derivatives/participant_dataframe.csv", sep = "")
 
 # directory where results will be saved
-res_dir = paste(proj_dir,'data/derivatives/results/LEgoBin/', sep='')
+res_dir <- paste(proj_dir, "data/derivatives/results/LEgoBin/", sep = "")
 
 # read in the df
 df <- read.csv(file = df_file)
 
 ## LESYMAP parameters
 # for now I am using mostly defaults, worth looking through the manual as
-# some of these choices interact, e.g., a univariate t-test method cannot use 
+# some of these choices interact, e.g., a univariate t-test method cannot use
 # the "voxel" dTVLC correction method
-min_subj = 10
-
-# generic template for plotting
-lesydata = file.path(find.package('LESYMAP'),'extdata')
-template = antsImageRead(Sys.glob(file.path(lesydata, 'template', 'ch2.nii.gz')))
-
-# univariate analysis on lesion maps
-
+min_subj <- 10
+correctByLesSize <- "behavior" # this is the typical lesion size behaviour regression
+multipleComparison <- "bonferroni"
+# univariate analysis on typical lesion maps
 # define the inputs
 X <- imageFileNames2ImageList(as.character(df$resampled_lesion_file))
 y <- as.numeric(df$LEgoBin)
 
-# run univariate lesion mapping 
-lsm = lesymap(X, y, 
-              method = 'BMfast', 
-              minSubjectPerVoxel = min_subj,
-              correctByLesSize = 'behavior',
-              saveDir = paste(res_dir,'univariate_lesion', sep=''))
+# run the VLSM
+lsm <- lesymap(X, y,
+    method = "BMfast",
+    multipleComparison = multipleComparison,
+    minSubjectPerVoxel = min_subj,
+    correctByLesSize = correctByLesSize,
+    saveDir = paste(res_dir, "univariate_lesion", sep = "")
+)
 
-# plot the stat map if you are running interactively
-plot(template, lsm$stat.img)
+# univariate analysis on connectivity data
+# change the X input
+X <- imageFileNames2ImageList(as.character(df$connectivity_lesion_normalised_binarized80_file))
 
-# run multivariate lesion mapping 
-lsm = lesymap(X, y, 
-              method = 'sccan', 
-              minSubjectPerVoxel = min_subj,
-              correctByLesSize = 'behavior',
-              saveDir = paste(res_dir,'univariate_lesion', sep=''))
-
-# plot the stat map if you are running interactively
-plot(template, lsm$stat.img)
+# run the VLSM
+lsm <- lesymap(X, y,
+    method = "BMfast",
+    multipleComparison = multipleComparison,
+    minSubjectPerVoxel = min_subj,
+    correctByLesSize = correctByLesSize,
+    saveDir = paste(res_dir, "univariate_connectivity", sep = "")
+)
